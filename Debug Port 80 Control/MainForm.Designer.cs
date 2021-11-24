105f5af0e0617f912ab596234ae5bae5e5eaf0e1
Sliicy <Sliicy@users.noreply.github.com>
2021-11-24 18:25:16 -0500

    Add project files.

---
+++ Debug Port 80 Control/MainForm.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace Debug_Port_80_Control {
+    partial class MainForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.tmrVolume = new System.Windows.Forms.Timer(this.components);
+            this.cboOption = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(32, 48);
+            this.txtInput.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(120, 31);
+            this.txtInput.TabIndex = 0;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(168, 44);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(150, 44);
+            this.btnSend.TabIndex = 1;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.BtnSend_Click);
+            // 
+            // tmrVolume
+            // 
+            this.tmrVolume.Interval = 1;
+            this.tmrVolume.Tick += new System.EventHandler(this.TmrVolume_Tick);
+            // 
+            // cboOption
+            // 
+            this.cboOption.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboOption.FormattingEnabled = true;
+            this.cboOption.Items.AddRange(new object[] {
+            "None",
+            "Volume",
+            "Keypress"});
+            this.cboOption.Location = new System.Drawing.Point(24, 152);
+            this.cboOption.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.cboOption.Name = "cboOption";
+            this.cboOption.Size = new System.Drawing.Size(238, 33);
+            this.cboOption.TabIndex = 4;
+            this.cboOption.SelectedIndexChanged += new System.EventHandler(this.CboOption_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 17);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(206, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Send to Debug Port:";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(817, 559);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cboOption);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtInput);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Debug Port 80 Control";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Timer tmrVolume;
+        private System.Windows.Forms.ComboBox cboOption;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Debug Port 80 Control/MainForm.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.CoreAudioApi;
+using OpenLibSys;
+
+namespace Debug_Port_80_Control {
+    public partial class MainForm : Form {
+        public MainForm() {
+            InitializeComponent();
+        }
+
+        // OpenLibSystem object used for read/writing to debug port:
+        Ols ols;
+
+        private int cachedVolumeLevel = 0;
+
+        private bool IsAdmin()
+        {
+            OperatingSystem osInfo = Environment.OSVersion;
+            if (osInfo.Platform == PlatformID.Win32Windows)
+            {
+                return true;
+            }
+            else
+            {
+                WindowsIdentity usrId = WindowsIdentity.GetCurrent();
+                WindowsPrincipal p = new WindowsPrincipal(usrId);
+                return p.IsInRole(@"BUILTIN\Administrators");
+            }
+        }
+
+        private bool RunAsRestart()
+        {
+            string[] args = Environment.GetCommandLineArgs();
+
+            foreach (string s in args)
+            {
+                if (s.Equals("runas"))
+                {
+                    return false;
+                }
+            }
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.UseShellExecute = true;
+            startInfo.WorkingDirectory = Environment.CurrentDirectory;
+            startInfo.FileName = Application.ExecutablePath;
+            startInfo.Verb = "runas";
+            startInfo.Arguments = "runas";
+
+            try
+            {
+                Process.Start(startInfo);
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e) {
+            if (!IsAdmin())
+            {
+                RunAsRestart();
+                Environment.Exit(0);
+            }
+            Init();
+        }
+
+        private void Init()
+        {
+            try
+            {
+                ols = new Ols();
+
+                // Check support library statuses:
+                switch (ols.GetStatus())
+                {
+                    case (uint)Ols.Status.NO_ERROR:
+                        break;
+                    case (uint)Ols.Status.DLL_NOT_FOUND:
+                        MessageBox.Show("Status Error!! DLL_NOT_FOUND");
+                        Environment.Exit(0);
+                        break;
+                    case (uint)Ols.Status.DLL_INCORRECT_VERSION:
+                        MessageBox.Show("Status Error!! DLL_INCORRECT_VERSION");
+                        break;
+                    case (uint)Ols.Status.DLL_INITIALIZE_ERROR:
+                        MessageBox.Show("Status Error!! DLL_INITIALIZE_ERROR");
+                        break;
+                }
+
+                // Check WinRing0 status:
+                switch (ols.GetDllStatus())
+                {
+                    case (uint)Ols.OlsDllStatus.OLS_DLL_NO_ERROR:
+                        break;
+                    case (uint)Ols.OlsDllStatus.OLS_DLL_DRIVER_NOT_LOADED:
+                        MessageBox.Show("DLL Status Error!! OLS_DRIVER_NOT_LOADED");
+                        Environment.Exit(0);
+                        break;
+                    case (uint)Ols.OlsDllStatus.OLS_DLL_UNSUPPORTED_PLATFORM:
+                        MessageBox.Show("DLL Status Error!! OLS_UNSUPPORTED_PLATFORM");
+                        Environment.Exit(0);
+                        break;
+                    case (uint)Ols.OlsDllStatus.OLS_DLL_DRIVER_NOT_FOUND:
+                        MessageBox.Show("DLL Status Error!! OLS_DLL_DRIVER_NOT_FOUND");
+                        Environment.Exit(0);
+                        break;
+                    case (uint)Ols.OlsDllStatus.OLS_DLL_DRIVER_UNLOADED:
+                        MessageBox.Show("DLL Status Error!! OLS_DLL_DRIVER_UNLOADED");
+                        Environment.Exit(0);
+                        break;
+                    case (uint)Ols.OlsDllStatus.OLS_DLL_DRIVER_NOT_LOADED_ON_NETWORK:
+                        MessageBox.Show("DLL Status Error!! DRIVER_NOT_LOADED_ON_NETWORK");
+                        Environment.Exit(0);
+                        break;
+                    case (uint)Ols.OlsDllStatus.OLS_DLL_UNKNOWN_ERROR:
+                        MessageBox.Show("DLL Status Error!! OLS_DLL_UNKNOWN_ERROR");
+                        Environment.Exit(0);
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+                Environment.Exit(0);
+            }
+        }
+
+        private void BtnSend_Click(object sender, EventArgs e)
+        {
+            ols.WriteIoPortWord(0x80, ushort.Parse(txtInput.Text));
+        }
+
+        private void tmrNumberLoop_Tick(object sender, EventArgs e)
+        {
+            ols.WriteIoPortWord(0x80, 0x01);
+            System.Threading.Thread.Sleep(700);
+            ols.WriteIoPortWord(0x80, 0x12);
+            System.Threading.Thread.Sleep(700);
+            ols.WriteIoPortWord(0x80, 0x23);
+            System.Threading.Thread.Sleep(700);
+            ols.WriteIoPortWord(0x80, 0x34);
+            System.Threading.Thread.Sleep(700);
+            ols.WriteIoPortWord(0x80, 0x45);
+            System.Threading.Thread.Sleep(700);
+            ols.WriteIoPortWord(0x80, 0x56);
+            System.Threading.Thread.Sleep(700);
+            ols.WriteIoPortWord(0x80, 0x67);
+            System.Threading.Thread.Sleep(700);
+            ols.WriteIoPortWord(0x80, 0x78);
+            System.Threading.Thread.Sleep(700);
+            ols.WriteIoPortWord(0x80, 0x89);
+            System.Threading.Thread.Sleep(700);
+            ols.WriteIoPortWord(0x80, 0x90);
+            System.Threading.Thread.Sleep(700);
+        }
+
+        // https://stackoverflow.com/questions/2534595/get-master-sound-volume-in-c-sharp
+        private int GetCurrentSpeakerVolume()
+        {
+            int volume = 0;
+            var enumerator = new MMDeviceEnumerator();
+            IEnumerable<MMDevice> speakDevices = enumerator.EnumerateAudioEndPoints(DataFlow.Render, DeviceState.Active).ToArray();
+            if (speakDevices.Count() > 0)
+            {
+                MMDevice mMDevice = speakDevices.ToList()[0];
+                volume = Convert.ToInt16(mMDevice.AudioEndpointVolume.MasterVolumeLevelScalar * 100);
+            }
+            return volume;
+        }
+
+        private void TmrVolume_Tick(object sender, EventArgs e)
+        {
+            if (cachedVolumeLevel == GetCurrentSpeakerVolume())
+            {
+                return;
+            }
+            int tens = GetCurrentSpeakerVolume() / 10;
+            int ones = GetCurrentSpeakerVolume() % 10;
+
+            switch (tens)
+            {
+                case 0:
+                    ols.WriteIoPortWord(0x80, (ushort)(ones));
+                    break;
+                case 1:
+                    ols.WriteIoPortWord(0x80, (ushort)(0x10 + ones));
+                    break;
+                case 2:
+                    ols.WriteIoPortWord(0x80, (ushort)(0x20 + ones));
+                    break;
+                case 3:
+                    ols.WriteIoPortWord(0x80, (ushort)(0x30 + ones));
+                    break;
+                case 4:
+                    ols.WriteIoPortWord(0x80, (ushort)(0x40 + ones));
+                    break;
+                case 5:
+                    ols.WriteIoPortWord(0x80, (ushort)(0x50 + ones));
+                    break;
+                case 6:
+                    ols.WriteIoPortWord(0x80, (ushort)(0x60 + ones));
+                    break;
+                case 7:
+                    ols.WriteIoPortWord(0x80, (ushort)(0x70 + ones));
+                    break;
+                case 8:
+                    ols.WriteIoPortWord(0x80, (ushort)(0x80 + ones));
+                    break;
+                case 9:
+                    ols.WriteIoPortWord(0x80, (ushort)(0x90 + ones));
+                    break;
+                default:
+                    ols.WriteIoPortWord(0x80, 0xFF);
+                    break;
+            }
+            cachedVolumeLevel = GetCurrentSpeakerVolume();
+        }
+
+        private void CboOption_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cboOption.Text == "Volume")
+            {
+                tmrVolume.Start();
+            } else
+            {
+                tmrVolume.Stop();
+            }
+        }
+    }
+}
