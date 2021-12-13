b29447a9bc312fadfdd5c64824efab1682fdc211
Sliicy <Sliicy@users.noreply.github.com>
2021-12-13 16:02:30 -0500

    Fix GUI Size

--- Debug Port 80 Control/MainForm.Designer.cs
+++ Debug Port 80 Control/MainForm.Designer.cs
@@ -99,7 +99,7 @@ namespace Debug_Port_80_Control {
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(596, 559);
+            this.ClientSize = new System.Drawing.Size(596, 126);
             this.Controls.Add(this.grpCustomSend);
             this.Controls.Add(this.grpEffect);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
